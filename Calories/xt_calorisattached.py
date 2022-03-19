d9deb3bf58041e17e996df8213ced4473b2e998e
lk20200413 <63585031+lk20200413@users.noreply.github.com>
2022-03-19 16:15:03 +0800

    Update xt_calorisattached.py

--- Calories/xt_calorisattached.py
+++ Calories/xt_calorisattached.py
@@ -1,33 +1,28 @@
 import sqlite3
-conn = sqlite3.connect('data.db',check_same_thread=False)
+
+conn = sqlite3.connect('data.db', check_same_thread=False)
 c = conn.cursor()
 
 
 def create_table():
-	c.execute('CREATE TABLE IF NOT EXISTS t_caloriss(Ingredients TEXT, Amount_of_Ingredients TEXT , Units TEXT,Note TEXT)')
+	c.execute('CREATE TABLE IF NOT EXISTS t_caloriss(Session_ID TEXT, Ingredients TEXT, Amount_of_Ingredients TEXT, Units TEXT, Note TEXT)')
 
 
-def add_data(ingredient,amount,units,note):
-	c.execute('INSERT INTO t_caloriss(Ingredients, Amount_of_Ingredients, Units, Note) VALUES (?,?,?,?)',(ingredient,amount,units,note))
+def add_data(session_id, ingredient,amount,units,note):
+	c.execute('INSERT INTO t_caloriss(Session_ID, Ingredients, Amount_of_Ingredients, Units, Note) VALUES (?,?,?,?)',(session_id, ingredient,amount,units,note))
 	conn.commit()
 
 
-def view_all_data():
-	c.execute('SELECT * FROM t_caloriss')
+def view_all_data(session_id):
+	c.execute(f'SELECT * FROM t_caloriss WHERE Session_ID="{session_id}"')
 	data = c.fetchall()
 	return data
 
-def view_all_ingredients_data():
-	c.execute('SELECT DISTINCT Ingredients FROM t_caloriss')
+def view_all_ingredients_data(session_id):
+	c.execute(f'SELECT DISTINCT Ingredients FROM t_caloriss WHERE Session_ID="{session_id}"')
 	data = c.fetchall()
 	return data
 
-def delete_data(ingredients1):
-	c.execute('DELETE FROM t_caloriss WHERE Ingredients="{}"'.format(ingredients1))
+def delete_data(session_id, ingredients):
+	c.execute(f'DELETE FROM t_caloriss WHERE Ingredients="{ingredients}" AND Session_ID="{session_id}"')
 	conn.commit()
-
-def edit_info_data(new_name,new_position_status,new_email,new_entry_date,Name,Position,Email,Entry_date):
-	c.execute("UPDATE t_empoyeetable SET Employee_Name =?,Position=?,Email_Address=?,Entry_date=? WHERE Employee_Name =? and Position=?and Email_Address=? and Entry_date=? ",(new_name,new_position_status,new_email,new_entry_date,Name,Position,Email,Entry_date))
-	conn.commit()
-	data = c.fetchall()
-	return data
