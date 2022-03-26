4cdb45f7b6ed307f331b899c409f4ea572d1d260
lk-itween <63585031+lk-itween@users.noreply.github.com>
2022-03-26 13:15:51 +0800

    Update xt_employee.py

--- Employee/xt_employee.py
+++ Employee/xt_employee.py
@@ -8,9 +8,6 @@ import streamlit as st
 from xt_employee_db import *
 
 
-def make_hashed(password):
-    return hashlib.sha256(str.encode(password)).hexdigest()
-
 st.set_page_config(initial_sidebar_state='expanded')
 st.title('Employee table App')
 st.markdown("""
@@ -22,12 +19,19 @@ You can create and edit your employee table, and **send email** to employee in t
 def user_table_init():
     create_user_table()
 
+@st.cache
+def make_hashed(password):
+    return hashlib.sha256(str.encode(password)).hexdigest()
+
 # 将登录状态设置成缓存状态，通过判断登录状态显示主页面
 if 'login_successfully' not in st.session_state:
     st.session_state.login_successfully = False
 
+# 将上一次成功登录的用户名保存至缓存
+if 'username' not in st.session_state:
+    st.session_state.username = ''
+
 user_table_init()
-employee_table_name = ''
 username = st.sidebar.text_input("User Name")
 password = st.sidebar.text_input("Password", type='password')
 
@@ -40,38 +44,39 @@ with sidecol2:
     signup_key = st.button('Sign Up')
 
 # 用户名及密码校验
-if all([password, username]):
-    hashed_pwd = make_hashed(password)
-    if login_key:
-        result = verify_pwd(username, hashed_pwd)
-        if result:
-            if not st.session_state.login_successfully:
-                st.sidebar.success(f"Logged in as {username}.")
-            employee_table_name = f'employee_table_{username}'
-        elif result is None:
-            st.sidebar.warning(f'User does not exist, please register!')
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
+def login_verify(username, password, login_key, signup_key):
+    if all([password, username]):
+        hashed_pwd = make_hashed(password)
+        if login_key:
+            result = verify_pwd(username, hashed_pwd)
+            if result:
+                if not st.session_state.login_successfully:
+                    st.sidebar.success(f"Logged in as {username}.")
+                return f'employee_table_{username}'
+            elif result is None:
+                st.sidebar.warning(f'User does not exist, please register!')
+            else:
+                st.sidebar.warning("Incorrect Username/Password !")
+        elif signup_key:
+            result = verify_pwd(username, hashed_pwd)
+            if result is None:
+                table_name = add_userdata(username, hashed_pwd)
+                st.sidebar.success(f'Successfully registered {username}!')
+                return table_name
+            elif result and st.session_state.login_successfully:
+                return f'employee_table_{username}'
+            else:
+                st.sidebar.info('The user name already exists, \nbut there is a problem with the password. \nPlease log in.')
         else:
-            st.sidebar.warning("Incorrect Username/Password !")
-    elif signup_key:
-        result = verify_pwd(username, hashed_pwd)
-        if result is None:
-            employee_table_name = add_userdata(username, hashed_pwd)
-            st.sidebar.success(f'Successfully registered {username}!')
-        elif result and st.session_state.login_successfully:
-            employee_table_name = f'employee_table_{username}'
-        else:
-            st.sidebar.info('The user name already exists, \nbut there is a problem with the password. \nPlease log in.')
-    else:
-        if st.session_state.login_successfully:
-            employee_table_name = f'employee_table_{username}'
-        else:
-            st.stop()
-else:
-    st.stop()
+            if st.session_state.login_successfully and username == st.session_state.username:
+                return f'employee_table_{username}'
 ########
-time.sleep(0.1)
-employee_create = employee_table_name.startswith('employee_table')
-st.session_state.login_successfully = employee_create
+employee_table_name = login_verify(username, password, login_key, signup_key)
+if not employee_table_name:
+    st.stop()
+st.session_state.username = username
+st.session_state.login_successfully = True
 
 # 获取用户设置的子表信息
 def view_all_employee(table_name):
@@ -81,7 +86,7 @@ def view_all_employee(table_name):
     return data
 
 # 主页面设置
-if st.session_state.login_successfully and employee_create:
+if st.session_state.login_successfully:
     with sidecol3:
         signout_key = st.button('Sign Out')
         if signout_key:
